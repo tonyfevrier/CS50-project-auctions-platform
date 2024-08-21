9241c1c92f2ab1d459ee31481f87764f8dc98315
FEVRIER <tony.fevrier62@gmail.com>
2024-08-21 16:08:37 -0400

    feat(models): create the listing model.

--- auctions/models.py
+++ auctions/models.py
@@ -1,6 +1,17 @@
 from django.contrib.auth.models import AbstractUser
-from django.db import models
+from django.db import models 
+
 
 
 class User(AbstractUser):
     pass
+
+
+class Listings(models.Model):
+    title = models.TextField(default="")
+    description = models.TextField(default="") 
+    price = models.FloatField(default=0.)
+    url = models.URLField(default="",null=True,blank=True)
+    category = models.TextField(default="",null=True,blank=True)
+    date = models.DateTimeField()
+
