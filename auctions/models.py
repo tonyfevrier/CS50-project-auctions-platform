b0eea27748806ed3cebfb2898e095013c254eebd
FEVRIER <tony.fevrier62@gmail.com>
2024-08-25 21:29:58 -0400

    feat(models): add to listing the Charfield winner.

--- auctions/models.py
+++ auctions/models.py
@@ -10,18 +10,19 @@ class User(AbstractUser):
 
 
 class Listings(models.Model):
-    title = models.TextField(default="")
+    title = models.CharField(default="",max_length=100)
     description = models.TextField(default="") 
     price = models.FloatField(default=0.)
     url = models.URLField(default="",null=True,blank=True)
-    category = models.TextField(default="",null=True,blank=True)
+    category = models.CharField(default="",null=True,blank=True,max_length=50)
     created_at = models.DateTimeField(default=timezone.now)  
     followers = models.JSONField(default=[])
-    creator = models.TextField(default="")
+    creator = models.CharField(default="",max_length=50)
+    winner = models.CharField(default="",max_length=50)
 
 
 class Bids(models.Model):
     price = models.FloatField(default=0.)
     listing = models.ForeignKey(Listings,on_delete=models.CASCADE)
-    bidder = models.TextField(default="")
+    bidder = models.CharField(default="",max_length=50)
 
